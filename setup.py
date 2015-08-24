20233a39dc6475c39d1ce983bb8d8dc5c1a9160d
Pablo Fuentes <pablo@studio73.es>
2015-08-24 11:26:08 +0200

    [ADD] Be able to install as a package

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#######################################
+#
+#        A Simple odoo connector
+#
+#           www.studio73.es
+#         contacto@studio73.es
+#
+#######################################
+
+from distutils.core import setup
+
+setup(name='odooconnector',
+      version='1.0',
+      description='A Simple Odoo connector',
+      author='Studio73',
+      author_email='contacto@studio73.es',
+      url='http://www.studio73.es',
+      py_modules=['odooconnector'],
+     )
